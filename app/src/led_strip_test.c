bf07dfa668337dd39ecd2cf7accdff27312b550f
Federico Roux <rouxfederico@gmail.com>
2026-01-30 19:34:54 -0300

    refactor(board): move board init to `board_init.c` (#18)
    Closes #13
    
    This PR is a big refactor. The overall idea is to prepare the repo to
    create a PR in the general Zephyr repository. So, in order to do that,
    the following changes are tackled here:
    
    * move all the pins and devices initialization to `board_init.c`
    * functions defined in `board_init.c` are not called by `main.c`, but
      added to automated Zephyr init stages, like `PRE_KERNEL_2` for board
    init, and `POST_KERNEL` for display init
    * expose all the pins and devices in `board_init.h`
    * separate all peripheral code and helpers to individual files if the
      device is not simple, like led strip or tft display mgmt
    * create a demo app in `main.c`
    
    Signed-off-by: Federico Roux <rouxfederico@gmail.com>
    
    stack-info: PR: https://github.com/fgr-17/heltec-t114-support/pull/18, branch: fgr-17/stack/11

---
+++ app/src/led_strip_test.c
@@ -0,0 +1,41 @@
+#include <zephyr/drivers/led_strip.h>
+#include <zephyr/logging/log.h>
+
+#include "board_init.h"
+#include "led_strip_test.h"
+
+LOG_MODULE_REGISTER(led_strip_test, LOG_LEVEL_DBG);
+
+#define COLOR_OFF   {.r = 0x00, .g = 0x00, .b = 0x00}
+#define COLOR_RED   {.r = 0xFF, .g = 0x00, .b = 0x00}
+#define COLOR_GREEN {.r = 0x00, .g = 0xFF, .b = 0x00}
+#define COLOR_BLUE  {.r = 0x00, .g = 0x00, .b = 0xFF}
+#define COLOR_WHITE {.r = 0xFF, .g = 0xFF, .b = 0xFF}
+
+struct led_rgb colors[] = {
+	COLOR_OFF, COLOR_RED, COLOR_GREEN, COLOR_BLUE, COLOR_WHITE, COLOR_OFF,
+};
+
+uint8_t pixel0_index = 0;
+uint8_t pixel1_index = 1;
+
+
+/**
+ * @brief Rotate the LED strip pixels
+ * @return 0 on success, 1 on failure
+ */
+ 
+int sk6812_pixel_rotate()
+{
+	struct led_rgb pixels[2] = {colors[pixel0_index], colors[pixel1_index]};
+
+	pixel0_index = (pixel0_index + 1) % ARRAY_SIZE(colors);
+	pixel1_index = (pixel1_index + 1) % ARRAY_SIZE(colors);
+
+	int ret = led_strip_update_rgb(led_strip, pixels, 2);
+	if (ret) {
+		LOG_ERR("LED update failed: %d", ret);
+		return 1;
+	}
+	return 0;
+}
